c20e7c806baa48c4a08f0ef9dde56b884574eed7
w7 <w7@192.168.0.3>
2020-06-12 08:06:16 +0530

    Test

---
+++ Demo/src/TestFor.java
@@ -0,0 +1,18 @@
+
+public class TestFor {
+	//Arrays,Collections
+	
+	public static void main(String[] args) {
+		int a[]= {1,2,3};
+		/*for(int i=0;i<a.length;i++)
+		{
+			System.out.println(a[i]);
+		}*/
+		
+		for(int i:a)
+		{
+			System.out.println(i);
+		}
+		
+	}
+}
